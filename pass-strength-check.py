107578712c993c5c37649fa91f1c6396ee01f4bd
Gloria <awesomegloria2@gmail.com>
2025-04-28 23:38:52 +0100

    Updated password checker with colour and improvements

--- pass-strength-check.py
+++ pass-strength-check.py
@@ -1,25 +1,40 @@
 import re
+from colorama import Fore, Style,init
+
+# Initialize colorama
+init()
 
 def check_password_strength(password):
     if len(password) < 8:
-        return "Weak: Password must be at least 8 characters long."
+        return  Fore.RED + "Weak: Password must be at least 8 characters long." + Style.RESET_ALL
     if not re.search(r"[A-Z]",password):
-        return "Weak: Password must contain at least one uppercase letter."
+        return Fore.RED + "Weak: Password must contain at least one uppercase letter." + Style.RESET_ALL
     if not re.search(r"[a-z]",password):
-        return "Weak: Password must contain at least one lowwercase letter."
+        return Fore.RED +  "Weak: Password must contain at least one lowwercase letter." + Style.RESET_ALL
     if not re.search(r"[0-9]",password):
-        return "Weak: Password must contain at least one digit."
+        return Fore.RED +  "Weak: Password must contain at least one digit." + Style.RESET_ALL
     if not re.search(r"[!@#$%^&*(),-.<>?;:'\"{}|_+=-]",password):
-        return "Weak: Password must contain at least one special character."
+        return Fore.RED +  "Weak: Password must contain at least one special character." + Style.RESET_ALL
     if re.search(r"\s",password):
-        return "Weak: Password must not contain any whitespace."
-    return "Strong: Password is strong."
+        return Fore.RED +  "Weak: Password must not contain any whitespace." + Style.RESET_ALL
+    return Fore.GREEN + "Strong: Password is strong." + Style.RESET_ALL
 
-#Inputpassword from user
-password = input("Enter a password to check:")
+def main():
 
-#Check password strength
-strength = check_password_strength(password)
-print(strength)
+    print(Fore.CYAN + "=== Password Strength Checker ===" + Style.RESET_ALL)
+    password = input("Enter your password:")
+    result = check_password_strength(password)
+    print(result)
 
-        
+if __name__ == "__main__":
+    main()
+
+
+#def main():
+  #  password = input("Enter a password to check its strength: ")
+   # result = check_password_strength(password)
+    
+    #if "Weak" in result:
+     #   print(Fore.RED + result)
+    #else:
+     #   print(Fore.GREEN + result)
